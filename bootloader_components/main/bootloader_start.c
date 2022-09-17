82939b527c54da6cdad40936752a1263538457b2
TheSomeMan <AlexeyN.Skvortsov@gmail.com>
2022-09-17 21:42:40 +0700

    Remove all unnecessary bootloader code, implement custom initialization

--- bootloader_components/main/bootloader_start.c
+++ bootloader_components/main/bootloader_start.c
@@ -9,10 +9,69 @@
 #include "bootloader_init.h"
 #include "bootloader_utility.h"
 #include "bootloader_common.h"
+#include "bootloader_mem.h"
+#include "bootloader_clock.h"
+#include "bootloader_console.h"
+#include "bootloader_flash_config.h"
+#include "bootloader_flash.h"
+#include "bootloader_flash_priv.h"
+#include <assert.h>
+#include "soc/cpu.h"
+#include "soc/soc.h"
+#include "soc/spi_reg.h"
+#include "soc/dport_reg.h"
+#include "rom/cache.h"
+#include "rom/spi_flash.h"
 
-static const char* TAG = "boot";
+static void bootloader_reset_mmu(void)
+{
+  /* completely reset MMU in case serial bootloader was running */
+  Cache_Read_Disable(0);
+#if !CONFIG_FREERTOS_UNICORE
+  Cache_Read_Disable(1);
+#endif
+  Cache_Flush(0);
+#if !CONFIG_FREERTOS_UNICORE
+  Cache_Flush(1);
+#endif
+  mmu_init(0);
+#if !CONFIG_FREERTOS_UNICORE
+  /* The lines which manipulate DPORT_APP_CACHE_MMU_IA_CLR bit are
+      necessary to work around a hardware bug. */
+  DPORT_REG_SET_BIT(DPORT_APP_CACHE_CTRL1_REG, DPORT_APP_CACHE_MMU_IA_CLR);
+  mmu_init(1);
+  DPORT_REG_CLR_BIT(DPORT_APP_CACHE_CTRL1_REG, DPORT_APP_CACHE_MMU_IA_CLR);
+#endif
 
-static int select_partition_number(bootloader_state_t *bs);
+  /* normal ROM boot exits with DROM0 cache unmasked,
+      but serial bootloader exits with it masked. */
+  DPORT_REG_CLR_BIT(DPORT_PRO_CACHE_CTRL1_REG, DPORT_PRO_CACHE_MASK_DROM0);
+#if !CONFIG_FREERTOS_UNICORE
+  DPORT_REG_CLR_BIT(DPORT_APP_CACHE_CTRL1_REG, DPORT_APP_CACHE_MASK_DROM0);
+#endif
+}
+
+void bootloader2_init(void)
+{
+  bootloader_init_mem();
+
+  // clear bss section
+  bootloader_clear_bss_section();
+
+  // bootst up vddsdio
+  bootloader_common_vddsdio_configure();
+
+  // reset MMU
+  bootloader_reset_mmu();
+
+  // config clock
+  bootloader_clock_configure();
+  // initialize uart console, from now on, we can use esp_log
+  bootloader_console_init();
+
+  // config WDT
+  bootloader_config_wdt();
+}
 
 /*
  * We arrive here after the ROM bootloader finished loading this second stage bootloader from flash.
@@ -21,41 +80,20 @@ static int select_partition_number(bootloader_state_t *bs);
  */
 void __attribute__((noreturn)) call_start_cpu0(void)
 {
-    // 1. Hardware initialization
-    if (bootloader_init() != ESP_OK) {
-        bootloader_reset();
-    }
+  bootloader2_init();
 
-#ifdef CONFIG_BOOTLOADER_SKIP_VALIDATE_IN_DEEP_SLEEP
-    // If this boot is a wake up from the deep sleep then go to the short way,
-    // try to load the application which worked before deep sleep.
-    // It skips a lot of checks due to it was done before (while first boot).
-    bootloader_utility_load_boot_image_from_deep_sleep();
-    // If it is not successful try to load an application as usual.
-#endif
+  // Switch the stack pointer to the end of RTC Slow memory
+  asm	(
+      "mov.n sp, %0\n"
+      :
+      :"r"(0x50001FFC)
+  );
 
-    // 2. Select the number of boot partition
-    bootloader_state_t bs = {0};
-    int boot_index = select_partition_number(&bs);
-    if (boot_index == INVALID_INDEX) {
-        bootloader_reset();
-    }
+  // Disable IRQs
+  XTOS_SET_INTLEVEL(XCHAL_EXCM_LEVEL);
 
-    // 3. Load the app image for booting
-    bootloader_utility_load_boot_image(&bs, boot_index);
-}
-
-// Select the number of boot partition
-static int select_partition_number(bootloader_state_t *bs)
-{
-    // 1. Load partition table
-    if (!bootloader_utility_load_partition_table(bs)) {
-        ESP_LOGE(TAG, "load partition table error!");
-        return INVALID_INDEX;
-    }
-
-    // 2. Select the number of boot partition
-    return bootloader_utility_get_selected_boot_partition(bs);
+  while (1) {
+  }
 }
 
 // Return global reent struct if any newlib functions are linked to bootloader
